dcdcb57df97c77f326fd6c238af528913229f33d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-01-31 09:56:32 +0530

    as  operator aded

---
+++ OopsConcepts/OopsConcepts/OutExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsConcepts
+{
+
+    //ref vs out
+    class OutExample
+    {
+        static void Main(string[] args)
+        {
+            int index;
+           // Console.WriteLine("Previous value of index "+index.ToString());
+            string result= GetNameValueout(out index);
+            int refIndex=1;
+            GetNameValueref(ref refIndex);
+            Console.WriteLine("Current value of index " + index.ToString());
+           
+        }
+
+        public static string GetNameValueref(ref int id)
+        {
+            //string returnText = "DummyName" + id.ToString();
+            //id += 1;
+
+            return id.ToString();
+
+        }
+        public static string GetNameValueout(out int id)
+        {
+            //id = 1;
+            //string returnText = "DummyName" + id.ToString();
+            //id += 1;
+           // id = 1;
+            return id.ToString();
+
+        }
+    }
+}
---
+++ OopsConcepts/OopsConcepts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OopsConcepts
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+
+            string sampleString = "Demo class";
+
+            object obj = sampleString;
+            //As operator
+            string str2 = obj as string;
+            //type casting
+            // boxing and unboxing
+            //Created an object 
+            //object -->string
+            //
+            if (str2!=null)
+            {
+                Console.WriteLine("Successfully casted");
+                Console.WriteLine(str2);
+            }
+          
+        }
+    }
+}
