62491a04bcf3a3671754e1be4b88f86bf0d71b0c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-02 12:27:58 +0530

    added array of objects

--- OopsConecpts/OopsConecpts/Program.cs
+++ OopsConecpts/OopsConecpts/Program.cs
@@ -60,6 +60,7 @@ namespace OopsConecpts
             Console.WriteLine("Employee Name : {0}",EmployeeName);
             Console.WriteLine("Employee Salary : {0}",EmployeeSalary);
             Console.WriteLine("Employee Gender : {0}",EmployeeGender);
+           
 
         }
     }
@@ -72,9 +73,19 @@ namespace OopsConecpts
             //SamepleObject.GetValue();
             //SamepleObject.display();
 
-            Employee employee = new Employee();
-            employee.GetEmployeeInformation();
-            employee.ShowEmployeeData();
+            Employee []employee = new Employee[2];
+
+            for (int i = 0; i < employee.Length; i++)
+            {
+                Employee employee1 = new Employee();
+                employee1.GetEmployeeInformation();
+                employee[i] = employee1;
+            }
+            for (int i = 0; i < employee.Length; i++)
+            {
+                employee[i].ShowEmployeeData();
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
