c680b0318eefae64c1b96ab898398e4eef9870f1
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-03 09:53:01 +0530

    added inheritance

---
+++ InheritanceLeftOvers/InheritanceLeftOvers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InheritanceLeftOvers
+{
+
+    class A
+    {
+        int amember;
+        public A(int _amember)
+        {
+            amember = _amember;
+        }
+        public void PrintA()
+        {
+            Console.WriteLine(amember);
+        }
+    }
+    class B:A
+    {
+        int bmember;
+        public B(int _bmember):base(_bmember)
+        {
+            bmember = _bmember;
+        }
+        public void PrintB()
+        {
+            Console.WriteLine(bmember);
+        }
+    }
+    class C:B
+    {
+        int cmember;
+        public C(int _cmember) : base(_cmember)
+        {
+            cmember = _cmember;
+        }
+        public void PrintC()
+        {
+            Console.WriteLine(cmember);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            C cobject = new C(3);
+            cobject.PrintA();
+            cobject.PrintB();
+            cobject.PrintC();
+
+        }
+    }
+}
