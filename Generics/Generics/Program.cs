467165e3fb379570ab503d194d4943aff5513ca8
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-07 10:24:56 +0530

    Added generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+
+        // we use < > to specify the parameter type
+        //Generic class
+
+        public class sample<T>
+        {
+            //field
+            private T data;
+
+            //property
+            public T value
+            {
+                //accessors
+                get
+                {
+                    return this.data;
+                }
+                set
+                {
+                    this.data = value;
+                }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            sample<string> name = new sample<string>();
+            name.value = "vikash";
+
+            sample<float> version = new sample<float>();
+            version.value = 5.0F;
+            //display Name
+            Console.WriteLine(name.value);
+            //display version
+            Console.WriteLine(version.value);
+        }
+    }
+}
