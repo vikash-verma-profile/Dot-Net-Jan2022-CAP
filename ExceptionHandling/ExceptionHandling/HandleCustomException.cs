a5649e98a9b7d145c135ab3d212f8c839f02c171
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-07 13:02:34 +0530

    regex

--- ExceptionHandling/ExceptionHandling/HandleCustomException.cs
+++ ExceptionHandling/ExceptionHandling/HandleCustomException.cs
@@ -26,16 +26,24 @@ namespace ExceptionHandling
                 Console.WriteLine("Temperature :{0}", temperature);
             }
         }
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             TestTemprature testTemprature = new TestTemprature();
             try
             {
+                int a = 5;
+                int b = a / 0;
                 testTemprature.ShowTemperature();
             }
             catch (TempIsZeroException ex)
             {
                 Console.WriteLine("Custom execption is {0}", ex.Message); 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.StackTrace);
             }
         }
     }
---
+++ ExceptionHandling/ExceptionHandling/HandlingRegex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExceptionHandling
+{
+    internal class HandlingRegex
+    {
+        static void Main(string[] args)
+        {
+            //this will return any pattern line ab,aab,aaab;
+            //Regex re = new Regex("a*b");
+            //this will return pattern like b,ab
+            Regex re = new Regex("a?b");
+            Match match = re.Match("aaaaabcd");
+            if (match.Success)
+            {
+                Console.WriteLine("Mached the value "+ match.Value);
+            }
+        }
+    }
+}
