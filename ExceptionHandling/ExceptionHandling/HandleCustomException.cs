232ec39859502ba4f96ae241cf7efeefb21ff0da
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-07 11:16:39 +0530

    exception handing

---
+++ ExceptionHandling/ExceptionHandling/HandleCustomException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    public class TempIsZeroException : Exception
+    {
+        public TempIsZeroException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class TestTemprature
+    {
+        int temperature = 0;
+        public void ShowTemperature()
+        {
+            if (temperature == 0)
+            {
+                throw (new TempIsZeroException("Zero Temperature found"));
+            }
+            else
+            {
+                Console.WriteLine("Temperature :{0}", temperature);
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestTemprature testTemprature = new TestTemprature();
+            try
+            {
+                testTemprature.ShowTemperature();
+            }
+            catch (TempIsZeroException ex)
+            {
+                Console.WriteLine("Custom execption is {0}", ex.Message); 
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 5;
+            try
+            {
+                throw new Exception("Some error have occured");
+                //int b = a/0;
+                Console.WriteLine("Value of a is {0}", a);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally{
+                Console.WriteLine("Hi I am in finally block");
+            }
+        }
+    }
+}
