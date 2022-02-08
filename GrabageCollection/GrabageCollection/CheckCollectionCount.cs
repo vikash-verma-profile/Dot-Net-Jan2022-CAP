34aa5cfb84ae4f27f4a2f24eba86c584c787713a
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-08 12:09:34 +0530

    grabage collection code added

---
+++ GrabageCollection/GrabageCollection/CheckCollectionCount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrabageCollection
+{
+    class calculator
+    {
+        public int Add(int a,int b)
+        {
+            return (a + b);
+        }
+        public int Sub(int a, int b)
+        {
+            return (a - b);
+        }
+        public int Multi(int a, int b)
+        {
+            return (a * b);
+        }
+        public int Divide(int a, int b)
+        {
+            return (a / b);
+        }
+
+    }
+    internal class CheckCollectionCount
+    {
+        static void Main1(string[] args)
+        {
+            calculator calculator = new calculator();   
+            Console.WriteLine("Calculator object is now on "+GC.GetGeneration(calculator)+"Generation");
+            Console.WriteLine("Grabage Collection Occured at 0th Generation " + GC.GetGeneration(0));
+            Console.WriteLine("Grabage Collection Occured at 1th Generation " + GC.GetGeneration(1));
+            Console.WriteLine("Grabage Collection Occured at 2th Generation " + GC.GetGeneration(2));
+            GC.Collect(0);
+            Console.WriteLine("Grabage Collection Occured at 0th Generation " + GC.GetGeneration(0));
+        }
+    }
+}
--- GrabageCollection/GrabageCollection/Program.cs
+++ GrabageCollection/GrabageCollection/Program.cs
@@ -4,10 +4,19 @@ namespace GrabageCollection
 {
     internal class Program
     {
+        Program()
+        {
+            
+        }
+        ~Program()
+        {
+
+        }
         static void Main(string[] args)
         {
-            /*
+            
             Console.WriteLine("the generation are " + GC.GetTotalMemory(false));
+            /*
             Program p=new Program();
             Console.WriteLine("the generation are " +GC.MaxGeneration);
             Console.WriteLine("the generation are " + GC.GetGeneration(p));
@@ -16,9 +25,12 @@ namespace GrabageCollection
             //for collection of grabage 
             //gc.collect
 
-            GC.Collect();
-            Console.WriteLine("GC in generation 0 {0}",GC.CollectionCount(0));
-
+            //GC.Collect();
+            //Console.WriteLine("GC in generation 0 {0}",GC.CollectionCount(0));
+            finally
+            {
+                
+            }
 
         }
     }
