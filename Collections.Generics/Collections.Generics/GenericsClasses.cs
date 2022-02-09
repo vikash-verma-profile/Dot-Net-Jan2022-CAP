1c95e4289136bc2fe14eb3f220322e676b382f2d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-09 15:35:56 +0530

    first commit

--- Collections.Generics/Collections.Generics/GenericsClasses.cs
+++ Collections.Generics/Collections.Generics/GenericsClasses.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Collections;
 
 namespace Collections.Generics
@@ -28,19 +26,55 @@ namespace Collections.Generics
 
             //Hashtable is similar to arraylist but represents the items as a combination of a key and a value
 
-            Hashtable ht=new Hashtable();
-            ht.Add("vb","vb.net");
-            ht.Add("cs", "cs.net");
-            foreach (DictionaryEntry item in ht)
+            //Hashtable ht=new Hashtable();
+            //ht.Add("vb","vb.net");
+            //ht.Add("cs", "cs.net");
+            //foreach (DictionaryEntry item in ht)
+            //{
+            //    Console.WriteLine(item.Key);
+            //    Console.WriteLine(item.Value);
+            //}
+            //DictionaryEntry is a class whose object represents the data in a combination of key and value
+            
+
+            //its a class that has combintaion of arraylist and hashtable
+
+            SortedList Sl =new SortedList();
+
+            Sl.Add("1","JAVA");
+            Sl.Add("2", "C#");
+            Sl.Add("3", "C++");
+            //Sl.Capacity==> Capacity is the number of elements that we can have in sortedlist
+            //Sl.Count ==> count is the number of elemnets we have
+            //clear()
+            //containsvalue()
+            //containskey()
+            foreach (DictionaryEntry item in Sl)
             {
                 Console.WriteLine(item.Key);
                 Console.WriteLine(item.Value);
             }
-            //DictionaryEntry is a class whose object represents the data in a combination of key and value
-            
-            SortedList Sl =new SortedList();
+
+
+            //LIFO
             Stack s=new Stack();
+            s.Push("1");
+            s.Push("2");
+            s.Push("3");
+
+            foreach (object item in s)
+            {
+                Console.WriteLine(item);
+            }
+            //FIFO
             Queue q=new Queue();
+            q.Enqueue("1");
+            q.Enqueue("2");
+            q.Enqueue("3");
+            foreach (object item in q)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
