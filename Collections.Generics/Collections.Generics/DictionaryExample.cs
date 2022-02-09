ff4108faa551b7c5ae1982ca6070d43af500f65e
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-09 11:14:28 +0530

    first commit

--- Collections.Generics/Collections.Generics/DictionaryExample.cs
+++ Collections.Generics/Collections.Generics/DictionaryExample.cs
@@ -8,6 +8,7 @@ namespace Collections.Generics
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Example of Dictionary");
             Dictionary<int, string> PL = new Dictionary<int, string>();
             Console.WriteLine("Please enter name of languages");
@@ -19,7 +20,19 @@ namespace Collections.Generics
             {
                 Console.WriteLine(item.Key+"====>"+item.Value);
             }
-            
+            */
+            Console.WriteLine("Example of List");
+            List<string> list = new List<string>();
+            Console.WriteLine("Please enter name of languages");
+            for (int i = 1; i < 3; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
