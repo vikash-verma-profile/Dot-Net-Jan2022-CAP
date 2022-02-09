31985a7a8abb7b83d9e68285aaba5b4218b81e10
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-09 12:02:03 +0530

    first commit

--- Collections.Generics/Collections.Generics/DictionaryExample.cs
+++ Collections.Generics/Collections.Generics/DictionaryExample.cs
@@ -8,30 +8,58 @@ namespace Collections.Generics
     {
         static void Main(string[] args)
         {
-            /*
+            
             Console.WriteLine("Example of Dictionary");
             Dictionary<int, string> PL = new Dictionary<int, string>();
-            Console.WriteLine("Please enter name of languages");
-            for(int i = 1; i < 3; i++)
-            {
-               PL.Add(i,Console.ReadLine());
-            }
-            foreach (var item in PL)
-            {
-                Console.WriteLine(item.Key+"====>"+item.Value);
-            }
-            */
+            /* Console.WriteLine("Please enter name of languages");
+             for(int i = 1; i < 3; i++)
+             {
+                PL.Add(i,Console.ReadLine());
+             }
+             foreach (var item in PL)
+             {
+                 Console.WriteLine(item.Key+"====>"+item.Value);
+             }
+             */
+            
             Console.WriteLine("Example of List");
-            List<string> list = new List<string>();
+            List<int> list = new List<int>();
             Console.WriteLine("Please enter name of languages");
-            for (int i = 1; i < 3; i++)
+            for (int i = 1; i < 5; i++)
             {
-                list.Add(Console.ReadLine());
+                list.Add(Convert.ToInt32(Console.ReadLine()));
             }
+            Console.WriteLine("Before Sorting of List");
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
+            //clear all data in the list
+            //list.Clear();
+            list.Sort();
+            Console.WriteLine("After Sorting of List");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            //this will help us to find some value or search value in list
+            bool flag= list.Contains(9);
+            if (flag)
+            {
+                Console.WriteLine("We are able to find 9 in the list");
+            }
+            else
+            {
+                Console.WriteLine(" 9 was not there in the list");
+            }
+            list.Remove(1);
+            list.RemoveAt(1);
+            Console.WriteLine("After removal of items from List");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
 
         }
     }
