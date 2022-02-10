a00dfd38e009bf0d13f15cb2d38a42436806e9e4
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-10 11:25:22 +0530

    thread

---
+++ Threading/Threading/ExampleofThreadJoin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    internal class ExampleofThreadJoin
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(MethodJoin);
+            Console.WriteLine(thread.ThreadState);
+            thread.Start();
+            Console.WriteLine(thread.ThreadState);
+            thread.Join();
+            Console.WriteLine(thread.ThreadState);
+            Console.WriteLine("Work completed");
+        }
+
+        static void MethodJoin()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work in progress");
+            }
+        }
+    }
+}
---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //Thread th=Thread.CurrentThread;
+            //Console.WriteLine(th);
+
+            //example of asyncronous 
+            Thread work1Thread = new Thread(Work1);
+            Thread work2Thread = new Thread(Work2);
+            work1Thread.Start();
+            work2Thread.Start();    
+            //example of syncronous 
+            //Work1();
+            //Console.WriteLine("=======================================");
+            //Work2();
+            Console.ReadLine();
+        }
+        static void Work1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work 1 is called"+i.ToString());
+            }
+           
+        }
+        static void Work2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work 2 is called" + i.ToString());
+            }
+        }
+    }
+}
