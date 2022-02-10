c15ee8212b12dfe84a185c5b60692f1e31aa37cc
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-10 12:33:14 +0530

    threading

--- Threading/Threading/ExampleofThreadJoin.cs
+++ Threading/Threading/ExampleofThreadJoin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading;
 
@@ -9,13 +10,25 @@ namespace Threading
     {
         static void Main(string[] args)
         {
-            Thread thread = new Thread(MethodJoin);
-            Console.WriteLine(thread.ThreadState);
-            thread.Start();
-            Console.WriteLine(thread.ThreadState);
-            thread.Join();
-            Console.WriteLine(thread.ThreadState);
-            Console.WriteLine("Work completed");
+            //Thread thread = new Thread(MethodJoin);
+            //Console.WriteLine(thread.ThreadState);
+            //thread.Start();
+            //Console.WriteLine(thread.ThreadState);
+            //thread.Join();
+            //Console.WriteLine(thread.ThreadState);
+            //Console.WriteLine("Work completed");
+
+            Stopwatch stwatch = new Stopwatch();
+            stwatch.Start();
+
+            Thread oThread = new Thread(ProcessSleep);
+            oThread.Start();
+            oThread.Join();
+            stwatch.Stop(); 
+            TimeSpan ts=stwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",ts.Hours,ts.Minutes,ts.Seconds);
+            Console.WriteLine(elapsedTime);
+            Console.WriteLine("WOrk completed");
         }
 
         static void MethodJoin()
@@ -23,6 +36,14 @@ namespace Threading
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Work in progress");
+            }
+        }
+        static void ProcessSleep()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work in progress");
+                Thread.Sleep(4000);//4 second
             }
         }
     }
