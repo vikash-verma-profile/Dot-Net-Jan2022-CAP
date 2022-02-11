d2b7dbdbe0e1ce19bb2eac4ebb3747c33a2bdfc6
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-11 12:45:01 +0530

    first commit

--- TPLSample/TPLSample/Delegarteexample.cs
+++ TPLSample/TPLSample/Delegarteexample.cs
@@ -11,19 +11,23 @@ namespace TPLSample
     public delegate string GreetDelegate(string name);
     internal class Delegarteexample
     {
-        public void Add(int x,int y) {
-            Console.WriteLine(x+y);
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
         }
         public static string Greet(string name)
         {
             return "Hello "+name;
 
         }
-        static void Main(string[] args)
+        static void Main4(string[] args)
         {
+            //class object
             Delegarteexample obj =new Delegarteexample();
-
+            //Delgate instialization
             AddDelegate ad = new AddDelegate(obj.Add);
+
+
             GreetDelegate gd = new GreetDelegate(Delegarteexample.Greet);
             ad(100,50);
             Console.WriteLine(gd("Vikash"));
---
+++ TPLSample/TPLSample/MultiCastDelegate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPLSample
+{
+
+    //signature of multicast delegate
+
+    public delegate void RectangeDelete(double width,double height);
+    internal class MultiCastDelegate
+    {
+        public void GetArea(double width, double height)
+        {
+            Console.WriteLine("Area is :"+(width* height));
+        }
+        public void GetPerimeter(double width, double height)
+        {
+            Console.WriteLine("Parameter is :" + (2*(width + height)));
+        }
+        //5
+        static void Main(string[] args)
+        {
+            MultiCastDelegate rect = new MultiCastDelegate();
+            RectangeDelete rectange = new RectangeDelete(rect.GetArea);
+
+            //multicast
+            //you are pointing to two functions
+            //Added two methods as a refernce
+            rectange = rectange + rect.GetPerimeter;
+            //calling delegate
+            rectange.Invoke(12.56,67.78);
+            Console.WriteLine();
+            //removed one  methods refernce from multicast delegate
+            rectange = rectange - rect.GetPerimeter;
+            rectange.Invoke(12.56, 67.78);
+            Console.WriteLine();
+
+        }
+    }
+}
