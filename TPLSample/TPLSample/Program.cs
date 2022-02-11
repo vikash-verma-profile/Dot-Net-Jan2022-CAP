b3ce10ee908a7198768e8c2dd97541ed16fbde37
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-11 09:58:43 +0530

    added for vs parallel for

---
+++ TPLSample/TPLSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPLSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# for loop");
+            int number = 10;
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"value of count={i},{Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Parallel For loop");
+
+            Parallel.For(0, number, i =>
+            {
+                Console.WriteLine($"value of count={i}, {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+            });
+            Console.ReadLine();
+        }
+    }
+}
