bb064d675b8475ea029d408c6004dc10499ed16a
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-02-24 16:28:25 +0530

    added delted code

--- FirstWbAPI/FirstWbAPI/Controllers/HomeController.cs
+++ FirstWbAPI/FirstWbAPI/Controllers/HomeController.cs
@@ -12,20 +12,48 @@ namespace FirstWbAPI.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
-
+        static List<Student> studentList = new List<Student>();
+        [Route("getstudentdata")]
         [HttpGet]
         public List<Student> Get()
         {
-            List<Student> studentList = new List<Student>();
-            var student1 = new Student();
-            student1.StudentId = 1;
-            student1.StudentName ="Vikash Verma 1";
-            studentList.Add(student1);
-            var student2 = new Student();
-            student2.StudentId = 2;
-            student2.StudentName = "Vikash Verma 2";
-            studentList.Add(student2);
+
+            //var student1 = new Student();
+            //student1.StudentId = 1;
+            //student1.StudentName = "Vikash Verma 1";
+            //studentList.Add(student1);
+            //var student2 = new Student();
+            //student2.StudentId = 2;
+            //student2.StudentName = "Vikash Verma 2";
+            //studentList.Add(student2);
             return studentList;
         }
+
+        [Route("RecieveStudentData")]
+        [Consumes("application/x-www-form-urlencoded")]
+        [HttpPost]
+        public void RecieveStudentData([FromForm] Student studentdata)
+        {
+            studentList.Add(studentdata);
+        }
+
+
+        //[Route("poststudentdata")]
+        //[Consumes("application/x-www-form-urlencoded")]
+        //[HttpPost]
+        //public Student PostStudentData([FromForm] Student studentdata) {
+
+        //    return studentdata;
+
+
+        //}
+        //[HttpPost]
+        //[Route("postsample")]
+        //public string sampledata([FromBody]string sample)
+        //{
+        //    return sample;
+        //}
+
+
     }
 }
--- FirstWbAPI/FirstWbAPI/Startup.cs
+++ FirstWbAPI/FirstWbAPI/Startup.cs
@@ -26,6 +26,8 @@ namespace FirstWbAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient();
+            services.AddControllers().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
